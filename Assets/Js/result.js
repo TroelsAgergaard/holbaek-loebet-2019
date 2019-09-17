5c524e1350e9e594b4e3a7cb044e4b03bdffd462
Troels Agergaard <TroelsAgergaard@users.noreply.github.com>
2019-09-17 10:11:03 +0200

    Round to nearest quarter

--- Assets/Js/result.js
+++ Assets/Js/result.js
@@ -5,12 +5,12 @@ db.collection("rounds")
         querySnapshot.forEach(function(doc) {
             rounds.push(doc.data().round);
             //total += Math.round(doc.data().round * 10) / 10;
-            total += Math.round(doc.data().round * 2) / 2
+            total += Math.round(doc.data().round * 4) / 4
             // total += doc.data();
         });
         if(total < 0.25){
             document.querySelector('.fullscreen__result-header').innerHTML = 0;
         }else{
-            document.querySelector('.fullscreen__result-header').innerHTML = total.toFixed(1);
+            document.querySelector('.fullscreen__result-header').innerHTML = total.toFixed(2);
         }
 });
